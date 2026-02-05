27fcd1b3f366d99dd8f61f5add100164e6c3daf7
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-06 00:00:35 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Id(usize);
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Slot(usize);
 
+#[derive(PartialEq, Eq)]
 struct Renaming(Box<[Slot]>);
-struct RenamedId(Renaming, Id); // m*a
+
+impl Renaming {
+    // self * m2
+    // (self*m2)[x] = self[m2[x]]
+    fn compose(&self, m2: &Renaming) -> Renaming {
+        todo!()
+    }
+}
+
+// expresses m*a, where
+// - a :: X
+// - m :: X -> Y
+// - forall x :: X, m[x] :: Y
+#[derive(PartialEq, Eq)]
+struct RenamedId(/*m*/Renaming, /*a*/Id);
 
 impl Renaming {
     pub fn identity(arity: usize) -> Self {
         Self(
             (0..arity).map(Slot).collect()
         )
+    }
+
+    // m1 * (m2 * x)
+    // is (m1 * m2) * x
+    pub fn mul(&self, RenamedId(m2, x): &RenamedId) -> RenamedId {
+        let m1 = self;
+        todo!()
     }
 }
 
@@ -37,12 +59,25 @@ impl SlottedUF {
         i
     }
 
+    fn find(&self, RenamedId(mut m, mut a): RenamedId) -> RenamedId {
+        loop {
+            let RenamedId(m2, a2) = m.mul(&self.classes[a.0].leader);
+            if a == a2 { return RenamedId(m, a) }
+            RenamedId(m, a) = RenamedId(m2, a2);
+        }
+    }
+
     fn union(&mut self, x: RenamedId, y: RenamedId) {
+        let x = self.find(x);
+        let y = self.find(y);
         todo!()
     }
 
     fn is_equal(&self, x: RenamedId, y: RenamedId) -> bool {
-        todo!()
+        // NOTE incomplete due to groups!
+        let x = self.find(x);
+        let y = self.find(y);
+        x == y
     }
 }
 
