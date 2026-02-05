bc9a52b6de4d83197c9620c27717b0ac094153cb
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-05 23:44:48 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
+#[derive(Clone, Copy)]
 struct Id(usize);
+
+#[derive(Clone, Copy)]
 struct Slot(usize);
 
 struct Renaming(Box<[Slot]>);
 struct RenamedId(Renaming, Id); // m*a
 
+impl Renaming {
+    pub fn identity(arity: usize) -> Self {
+        Self(
+            (0..arity).map(Slot).collect()
+        )
+    }
+}
+
+struct Class {
+    group: (),
+    arity: usize,
+    leader: RenamedId, // equivalent to this Id.
+}
+
 struct SlottedUF {
+    classes: Vec<Class>,
 }
 
 impl SlottedUF {
     fn alloc(&mut self, arity: usize) -> Id {
-        todo!()
+        let m = Renaming::identity(arity);
+        let i = Id(self.classes.len());
+        self.classes.push(Class {
+            group: (),
+            arity,
+            leader: RenamedId(m, i),
+        });
+        i
     }
 
     fn union(&mut self, x: RenamedId, y: RenamedId) {
@@ -21,6 +46,4 @@ impl SlottedUF {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
