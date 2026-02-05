d8e576cda673676af1ee281f718c5e80ece84764
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-05 23:02:08 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct Id(usize);
+struct Slot(usize);
+
+struct Renaming(Box<[Slot]>);
+struct RenamedId(Renaming, Id); // m*a
+
+struct SlottedUF {
+}
+
+impl SlottedUF {
+    fn alloc(&mut self, arity: usize) -> Id {
+        todo!()
+    }
+
+    fn union(&mut self, x: RenamedId, y: RenamedId) {
+        todo!()
+    }
+
+    fn is_equal(&self, x: RenamedId, y: RenamedId) -> bool {
+        todo!()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
